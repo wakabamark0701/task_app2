1a278c98c6015aaa43e7bf63d8f1368b51c1cccb
wakabamark0701 <wakaani0701@gmail.com>
2021-04-21 10:30:10 +0900

    unix_socket　第二引数を空欄に戻す
    herokuにUPしたところエラーになったので
    第２引数を空欄に戻します。

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             // 'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
-            'unix_socket' => env('DB_SOCKET','/Applications/MAMP/tmp/mysql/mysql.sock'),
+            'unix_socket' => env('DB_SOCKET',''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
