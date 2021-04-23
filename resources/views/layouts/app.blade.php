23acacec9d88c2bbabc05c99f87e8177b390f44f
wakabamark0701 <wakaani0701@gmail.com>
2021-04-23 10:06:32 +0900

    ログイン表示修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,8 +69,8 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                <a id="navbarDropdown" class="nav-link " href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}がﾛｸﾞｲﾝ中<span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
